86aadae39c5b8b801f1a75e5dc645323857edbe1
StephSalisbury <estefania.salisbury@gmail.com>
2019-10-31 15:38:14 -0600

    Configuraciones checking ui

--- app/src/main/java/com/example/my_spender/ui/configuraciones/ConfiguracionesFragment.java
+++ app/src/main/java/com/example/my_spender/ui/configuraciones/ConfiguracionesFragment.java
@@ -5,12 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.my_spender.R;
 
 public class ConfiguracionesFragment extends Fragment {
-
+    @Override
+    @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_configuraciones, container, false);
