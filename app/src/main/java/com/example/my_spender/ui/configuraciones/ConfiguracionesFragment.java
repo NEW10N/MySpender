e2e72018556e6746692b73b82a57dd40cf9ce61c
NEW10N <cesarcastro10@outlook.com>
2019-11-01 09:01:42 -0600

    Se agrego la funcionalidad de los botones en las configuraciones.

--- app/src/main/java/com/example/my_spender/ui/configuraciones/ConfiguracionesFragment.java
+++ app/src/main/java/com/example/my_spender/ui/configuraciones/ConfiguracionesFragment.java
@@ -4,19 +4,64 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.my_spender.R;
+import com.example.my_spender.SistemaDifusoCachorro;
 
 public class ConfiguracionesFragment extends Fragment {
-    @Override
+
     @Nullable
+
+    Button btnComenzar;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_configuraciones, container, false);
+
+        radioGroup = (RadioGroup) root.findViewById(R.id.radioGroup);
+        btnComenzar = (Button) root.findViewById(R.id.buttonComenzar);
+
+        addListenerOnButton();
         return root;
+    }
+    public void addListenerOnButton(){
+
+        btnComenzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //get selected radio button from radioGroup
+                int selectedID = radioGroup.getCheckedRadioButtonId();
+
+                //find the radiobutton by returned id
+                radioButton = (RadioButton) getActivity().findViewById(selectedID);
+                if (radioGroup.getCheckedRadioButtonId() == -1) {
+                    Toast.makeText(getContext(), "Elegir una opción.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if (radioButton.getText().toString().trim().equalsIgnoreCase("Cachorro")) {
+                        Toast.makeText(getContext(), "Has elegido cachorro.", Toast.LENGTH_SHORT).show();
+                        //  FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                        //transaction.replace(getContext(),);
+                    }
+                    if (radioButton.getText().toString().trim().equalsIgnoreCase("Adulto")) {
+                        Toast.makeText(getContext(), "Has elegido adulto.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
 }
