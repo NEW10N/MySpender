9e1ab8150cba1b8f315b9617d6b59a4d27a9f739
Karla GH <KarlaHdz.98@outlook.com>
2019-11-10 18:22:44 -0600

    Modo uso primer avance

---
+++ app/src/main/java/com/example/my_spender/ui/modo/ModoUsoConectividadFragment.java
@@ -0,0 +1,32 @@
+package com.example.my_spender.ui.modo;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.my_spender.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ModoUsoConectividadFragment extends Fragment {
+
+
+    public ModoUsoConectividadFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_modo_uso_conectividad, container, false);
+    }
+
+}
